67b1e5590b43e62c8828c7ccd713d2060161a148
James Sully <sullyj3@gmail.com>
2023-07-31 22:17:05 +1000

    Trim down flake

--- flake.nix
+++ flake.nix
@@ -1,34 +1,18 @@
 {
   description = "A Rust web server including a NixOS module";
-
-  # Nixpkgs / NixOS version to use.
   inputs.nixpkgs.url = "nixpkgs/nixos-23.05";
-
   inputs.import-cargo.url = github:edolstra/import-cargo;
 
   outputs = { self, nixpkgs, import-cargo }:
     let
-
       # to work with older version of flakes
       lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
-
-      # Generate a user-friendly version number.
       version = "${builtins.substring 0 8 lastModifiedDate}-${self.shortRev or "dirty"}";
-
-      # System types to support.
       supportedSystems = [ "x86_64-linux" ];
-
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-
-      # Nixpkgs instantiated for supported system types.
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
-
     in {
-
-      # A Nixpkgs overlay.
       overlay = final: prev: {
-
         rust-web-server = with final; final.callPackage ({ inShell ? false }: stdenv.mkDerivation rec {
           name = "rust-web-server-${version}";
 
@@ -68,26 +52,17 @@
 
       };
 
-      # Provide some binary packages for selected system types.
       packages = forAllSystems (system:
         {
           inherit (nixpkgsFor.${system}) rust-web-server;
         });
 
-      # The default package for 'nix build'. This makes sense if the
-      # flake provides only one package or there is a clear "main"
-      # package.
       defaultPackage = forAllSystems (system: self.packages.${system}.rust-web-server);
-
-      # Provide a 'nix develop' environment for interactive hacking.
       devShell = forAllSystems (system: self.packages.${system}.rust-web-server.override { inShell = true; });
 
-      # A NixOS module.
-      nixosModules.rust-web-server =
-        { pkgs, ... }:
+      nixosModules.rust-web-server = { pkgs, ... }:
         {
           nixpkgs.overlays = [ self.overlay ];
-
           systemd.services.rust-web-server = {
             wantedBy = [ "multi-user.target" ];
             serviceConfig.ExecStart = "${pkgs.rust-web-server}/bin/rust-web-server";
@@ -98,7 +73,6 @@
       checks = forAllSystems
         (system:
           with nixpkgsFor.${system};
-
           {
             inherit (self.packages.${system}) rust-web-server;
 
